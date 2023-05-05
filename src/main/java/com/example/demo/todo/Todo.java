4199d219cd3e6275eac4e95390ea9967b74e0fc8
Jan <jan.krol.legowski@gmail.com>
2023-05-05 16:01:05 +0200

    add delete, update buttons and validations

--- src/main/java/com/example/demo/todo/Todo.java
+++ src/main/java/com/example/demo/todo/Todo.java
@@ -1,6 +1,9 @@
 package com.example.demo.todo;
 
 import java.time.LocalDate;
+import jakarta.validation.constraints.Size;
+
+
 
 public class Todo {
 
@@ -15,6 +18,8 @@ public class Todo {
 
     private int id;
     private String username;
+
+    @Size(min=10, message="Enter atleast 10 characters")
     private String description;
     private LocalDate targetDate;
     private boolean done;
--- src/main/java/com/example/demo/todo/TodoService.java
+++ src/main/java/com/example/demo/todo/TodoService.java
@@ -1,25 +1,51 @@
 package com.example.demo.todo;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 @Service
 public class TodoService {
 
 
     private static List<Todo> todos = new ArrayList<>();
+    private  static  int todosCount=0;
     static {
-        todos.add(new Todo(1, "in28minutes","Learn AWS",
+        todos.add(new Todo(++todosCount, "in28minutes","Learn AWS",
                 LocalDate.now().plusYears(1), false ));
-        todos.add(new Todo(2, "in28minutes","Learn DevOps",
+        todos.add(new Todo(++todosCount, "in28minutes","Learn DevOps",
                 LocalDate.now().plusYears(2), false ));
-        todos.add(new Todo(3, "in28minutes","Learn Full Stack Development",
+        todos.add(new Todo(++todosCount, "in28minutes","Learn Full Stack Development",
                 LocalDate.now().plusYears(3), false ));
     }
 
     public static List<Todo> getTodos() {
-        return todos;
+        return todos.stream().sorted(Comparator.comparingInt(Todo::getId)).collect(Collectors.toList());
+    }
+
+    public void addTodo(String username,String description,LocalDate targetDate, Boolean done ){
+        Todo todo = new Todo(++todosCount,username,description,targetDate,done);
+        todos.add(todo);
+    }
+
+    public void deleteById(int id) {
+        //todo.getId() == id
+        // todo -> todo.getId() == id
+        Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+        todos.removeIf(predicate);
+    }
+
+    public Todo findById(int id) {
+        Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+        Todo todo = todos.stream().filter(predicate).findFirst().get();
+        return todo;
+    }
+    public void updateTodo(@Valid Todo todo){
+        deleteById(todo.getId());
+        todos.add(todo);
     }
 }
